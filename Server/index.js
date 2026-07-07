a68dc8246a5452194039aed53b284bba6cbbd4e4
Prince15 <princetandel2006@gmail.com>
2026-07-07 17:58:21 +0530

    fix(server): increase json and urlencoded body parser size limits to 25mb for logo uploads

--- Server/index.js
+++ Server/index.js
@@ -56,7 +56,8 @@ const privateCors = cors({
     origin: "*",
   });
 
-app.use(express.json())
+app.use(express.json({ limit: "25mb" }))
+app.use(express.urlencoded({ limit: "25mb", extended: true }))
 app.use(cookieParser())
 
 
